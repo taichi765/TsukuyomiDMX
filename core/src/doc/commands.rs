dbb3878b46bfe97b2146a3607e565bb9778fc3ec
taichi765 <taichi0209.youtub@gmail.com>
2026-01-29 21:05:01 +0900

    refactor: Move some features from crate::commands to crate::doc::commands
    - Re-defined DocCommand trait: DocCommand::apply() consumes self, so it
      can't be called multiple times.
      Remove DocCommand::revert() and apply() returns reverse command. This
      change reduces DRY.
    
    - Manage undo/redo stack in Doc, not in CommandManager

---
+++ core/src/doc/commands.rs
@@ -0,0 +1,114 @@
+pub use defs::*;
+pub use fixtures::*;
+pub use functions::*;
+pub use plugins::*;
+pub use universes::*;
+
+use crate::doc::{DocEffect, state::DocState};
+
+pub trait DocCommand {
+    /// 逆コマンドを返す。
+    fn apply(self, state: &DocState) -> (Box<dyn DocCommand>, DocEffect);
+}
+
+mod fixtures {
+    use crate::doc::state::DocState;
+    use crate::doc::{DocCommand, DocEffect};
+    use crate::fixture::{Fixture, FixtureChange, FixtureId};
+
+    pub struct AddFixtureCommand {
+        fixture: Fixture,
+    }
+
+    impl AddFixtureCommand {
+        pub fn new(fixture: Fixture) -> Self {
+            Self { fixture }
+        }
+    }
+
+    impl DocCommand for AddFixtureCommand {
+        fn apply(self, state: &DocState) -> (Box<dyn DocCommand + 'static>, DocEffect) {
+            let id = self.fixture.id();
+            state.with_fixtures_mut(|it| it.insert(id, self.fixture));
+            (
+                Box::new(RemoveFixtureCommand::new(id)),
+                DocEffect::FixtureAdded(self.id),
+            )
+        }
+    }
+
+    pub struct UpdateFixtureCommand {
+        id: FixtureId,
+        change: FixtureChange,
+    }
+
+    impl UpdateFixtureCommand {
+        pub fn new(id: FixtureId, change: FixtureChange) -> Self {
+            Self { id, change }
+        }
+    }
+
+    impl DocCommand for UpdateFixtureCommand {
+        fn apply(self, state: &DocState) -> (Box<dyn DocCommand + 'static>, DocEffect) {
+            let rev_change = state.with_fixtures(|it| {
+                let fxt = it.get(&self.id).unwrap();
+                self.change.inverse_from(fxt)
+            });
+            state.with_fixtures_mut(|it| {
+                it.get_mut(&self.id).unwrap().apply_change(self.change);
+            });
+            (
+                Box::new(UpdateFixtureCommand::new(self.id, rev_change)),
+                DocEffect::FixtureUpdated(self.id),
+            )
+        }
+    }
+
+    pub struct RemoveFixtureCommand {
+        id: FixtureId,
+    }
+
+    impl RemoveFixtureCommand {
+        pub fn new(id: FixtureId) -> Self {
+            Self { id }
+        }
+    }
+
+    impl DocCommand for RemoveFixtureCommand {
+        fn apply(self, state: &DocState) -> (Box<dyn DocCommand + 'static>, DocEffect) {
+            let removed = state.with_fixtures_mut(|it| it.remove(&self.id).unwrap());
+            (
+                Box::new(AddFixtureCommand::new(removed)),
+                DocEffect::FixtureRemoved(self.id),
+            )
+        }
+    }
+}
+
+mod defs {
+    pub struct AddFixtureDefCommand {}
+
+    pub struct UpdateFixtureDefCommand {}
+
+    pub struct RemoveFixtureDefCommand {}
+}
+
+mod functions {
+    pub struct AddFunctionCommand {}
+
+    pub struct UpdateFunctionCommand {}
+
+    pub struct RemoveFunctionCommand {}
+}
+
+mod plugins {
+    pub struct AddOutputCommand {}
+
+    pub struct RemoveOutputCommand {}
+}
+
+mod universes {
+    pub struct AddUniverseCommand {}
+
+    pub struct RemoveUniverseCommand {}
+}
