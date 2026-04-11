b1e9b2d3b52c24dd8cc1092f466ef55cb91da82e
taichi765 <taichi0209.youtub@gmail.com>
2026-04-11 11:08:18 +0900

    feat: Enable log filtering with EnvFilter

--- crates/tsukuyomidmx/src/lib.rs
+++ crates/tsukuyomidmx/src/lib.rs
@@ -23,8 +23,7 @@ use i_slint_backend_winit::WinitWindowAccessor;
 use slint::wgpu_28::{WGPUConfiguration, WGPUSettings};
 use slint::{Timer, TimerMode};
 use tracing::Level;
-use tracing_subscriber::FmtSubscriber;
-use tracing_subscriber::fmt::format::FmtSpan;
+use tracing_subscriber::{EnvFilter, FmtSubscriber, fmt, prelude::*};
 use tsukuyomidmx_core::engine::{Engine, EngineCommand, EngineMessage};
 use tsukuyomidmx_core::prelude::*;
 
@@ -35,12 +34,11 @@ mod ui {
 }
 
 pub fn run_main() -> Result<(), Box<dyn Error>> {
-    // TODO: TSUKUYOMIDMX_LOG_LEVELで指定できるようにする
     // Initialize logger
-    let subscriber = FmtSubscriber::builder()
-        .with_max_level(Level::DEBUG)
-        .finish();
-    tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
+    tracing_subscriber::registry()
+        .with(fmt::layer())
+        .with(EnvFilter::from_default_env())
+        .init();
 
     // Use wgpu to render 3D Preview
     slint::BackendSelector::new()
