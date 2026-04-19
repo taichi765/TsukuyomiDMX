22e4d129659311a346cad594f3089164a58d60f1
taichi765 <taichi0209.youtub@gmail.com>
2026-04-19 11:47:45 +0900

    chore(deploy): Add AppImage

---
+++ scripts/create-appimage.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+~/Applications/linuxdeploy-x86_64.AppImage --appdir=TsukuyomiDmx-linux-x86_64.AppDir --executable=./target/release/tsukuyomidmx --desktop-file=./platforms/linux/TsukuyomiDmx.desktop --icon-file=./tsukuyomidmx.png  --output=appimage
