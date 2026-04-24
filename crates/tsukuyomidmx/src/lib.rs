adfe7fd43e169f7299e4ccf580336eda14d14418
taichi765 <taichi0209.youtub@gmail.com>
2026-04-25 00:12:16 +0900

    feat: Add Observable type

--- crates/tsukuyomidmx/src/lib.rs
+++ crates/tsukuyomidmx/src/lib.rs
@@ -86,3 +86,118 @@ pub fn run_main() -> Result<(), Box<dyn Error>> {
     app.run()?;
     Ok(())
 }
+
+mod observable {
+    use i_slint_core::model::{ModelChangeListener, ModelChangeListenerContainer};
+    use slint::{ModelNotify, ModelTracker};
+    use std::{cell::RefCell, pin::Pin, rc::Rc};
+
+    /// Observable data
+    ///
+    /// # Example
+    /// ```
+    /// # use std::rc::Rc;
+    /// # use std::cell::Cell;
+    /// use tsukuyomidmx::Observable;
+    ///
+    /// let count = Observable::new(0);
+    /// let double = Rc::new(Cell::new(0));
+    /// count.subscribe({
+    ///     let double = Rc::clone(&double);
+    ///     move |c| double.set(c * 2)
+    /// });
+    ///
+    /// count.set(2);
+    /// assert_eq!(double.get(), 4);
+    ///
+    /// count.update(|v| v + 3);
+    /// assert_eq!(double.get(), 10);
+    /// ```
+    pub struct Observable<T: 'static>(Rc<RefCell<ObservableInner<T>>>);
+
+    struct ObservableInner<T: 'static> {
+        data: T,
+        peer_containers: Vec<Pin<Box<ModelChangeListenerContainer<Peer<T>>>>>,
+        // TODO: slintのシステムを使う必要あるか？
+        notify: ModelNotify,
+    }
+
+    impl<T> Observable<T>
+    where
+        T: 'static,
+    {
+        pub fn new(data: T) -> Self {
+            Self(Rc::new(RefCell::new(ObservableInner {
+                data: data,
+                peer_containers: Vec::new(),
+                notify: ModelNotify::default(),
+            })))
+        }
+
+        /// Sets the new value and notifies to subscribers.
+        pub fn set(&self, val: T) {
+            self.0.borrow_mut().data = val;
+            self.0.borrow().notify.row_changed(0);
+        }
+
+        /// Updates value based on current value and notifies to subscribers.
+        pub fn update<F>(&self, f: F)
+        where
+            F: FnOnce(&T) -> T,
+        {
+            let new = f(&self.0.borrow().data);
+            self.0.borrow_mut().data = new;
+            self.0.borrow().notify.row_changed(0);
+        }
+
+        pub fn subscribe<F>(&self, f: F)
+        where
+            F: FnMut(&T) + 'static,
+        {
+            let container = Box::pin(ModelChangeListenerContainer::new(Peer {
+                val: Self(Rc::clone(&self.0)),
+                f: RefCell::new(Box::new(f)),
+            }));
+
+            self.0
+                .borrow()
+                .notify
+                .attach_peer(container.as_ref().model_peer());
+            self.0.borrow_mut().peer_containers.push(container);
+        }
+    }
+
+    impl<T> Clone for Observable<T> {
+        /// This is cheap clone (same as [`Rc::clone()`][std::rc::Rc])
+        fn clone(&self) -> Self {
+            Self(Rc::clone(&self.0))
+        }
+    }
+
+    struct Peer<T: 'static> {
+        val: Observable<T>,
+        f: RefCell<Box<dyn FnMut(&T) + 'static>>,
+    }
+
+    impl<T> ModelChangeListener for Peer<T>
+    where
+        T: 'static,
+    {
+        fn row_added(self: Pin<&Self>, _index: usize, _count: usize) {
+            unimplemented!("row_added() is never called");
+        }
+
+        fn row_changed(self: Pin<&Self>, row: usize) {
+            debug_assert_eq!(0, row);
+            (self.f.borrow_mut())(&self.val.0.borrow().data)
+        }
+
+        fn row_removed(self: Pin<&Self>, _index: usize, _count: usize) {
+            unimplemented!("row_removed() is never called");
+        }
+
+        fn reset(self: Pin<&Self>) {
+            unimplemented!("reset() is never called");
+        }
+    }
+}
